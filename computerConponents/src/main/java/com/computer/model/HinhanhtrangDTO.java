67ec6bb0c377a083ccadec024ad1462ee61f89d1
Hoang Tuan <34570205+goodgguy@users.noreply.github.com>
2020-08-02 16:27:42 +0700

    test

---
+++ computerConponents/src/main/java/com/computer/model/HinhanhtrangDTO.java
@@ -0,0 +1,11 @@
+package com.computer.model;
+
+public class HinhanhtrangDTO {
+	private int idHinh;
+	private String footer1;
+	private String footer2;
+	private String footer3;
+	private String header1;
+	private String header2;
+	
+}
